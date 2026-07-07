19dc7f75505b1594d4c3458a546c46aa8e24919d
Dante2z9 <anubhavjatin2907@gmail.com>
2026-07-08 01:36:42 +0530

    Time: 0 ms (100%), Space: 52.8 MB (76.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,7 +4,7 @@ class Solution
     {
         int low = 0;
         int high = nums.length-1;
-        while (low < high)
+        while (low < high) //the reason why we dont use <= is because we want that when high and low are equal return either high or low for ex at a point high = low =4 that means only a single element is left return that . But the moment we use less that or equal since 4==4 the looop will run one more time now image there were only 5 element that is index was at 4 and now u did if nums[mid]==nums[mid+1] i.e nums[5] which doesnt even exist so out of bound error
         {
             int mid = low + (high-low)/2;
             if(mid%2 == 1)
