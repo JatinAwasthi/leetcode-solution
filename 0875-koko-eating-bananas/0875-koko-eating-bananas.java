a91b21a682b9c945a9d07c1eb9e19694968d77ca
Dante2z9 <anubhavjatin2907@gmail.com>
2026-07-09 12:43:32 +0530

    Time: 22 ms (29.16%), Space: 48.5 MB (5.72%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,42 @@
+class Solution 
+{
+    public int minEatingSpeed(int[] piles, int h) 
+    {
+        int len = piles.length;
+        int max = piles[0];
+        
+            
+        
+        for (int i = 1; i < piles.length; i++) 
+        {
+            if (piles[i] > max) 
+            {
+                max = piles[i];
+            }
+        }
+        int lowSpeed = 1; //low here is the speed and it cant be 0
+        int highSpeed = max;
+        int ans = max;
+        while(lowSpeed<=highSpeed)
+        {
+            int midSpeed = lowSpeed + (highSpeed - lowSpeed) / 2;
+
+            long total = 0 ;
+            for(int i = 0 ; i<piles.length ; i++)
+            {
+                int hours = (int) Math.ceil((double) piles[i] / midSpeed);
+                total+=hours;
+            }
+            if(total <= h)
+            {
+                ans = midSpeed;
+                highSpeed = midSpeed - 1;
+            }
+            else{
+                lowSpeed = midSpeed+1;
+            }
+        }
+
+        return ans;
+    }
+}
